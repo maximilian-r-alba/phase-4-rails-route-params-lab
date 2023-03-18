bc173c3608398cdd05b5a3d6e7073465b1b8f2b5
Maximilian Rex Alba <albam476@gmail.com>
2023-03-18 15:50:01 -0400

    commit tree f468439681f95b343eb90327952548a49120b93e

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,7 +2,17 @@ class StudentsController < ApplicationController
 
   def index
     students = Student.all
+    if(params[:name])
+      filteredStudents = Student.all.filter {|s| s.first_name.downcase == params[:name] || s.last_name.downcase == params[:name]}
+      render json: filteredStudents
+      byebug
+  else
     render json: students
   end
+  end
 
+  def show
+    student = Student.find(params[:id])
+    render json: student
+  end
 end
